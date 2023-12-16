76d3d4642d5777c1919af6a39acc47594f6cfe06
rabin2403 <rabindra2402@gmail.com>
2023-12-16 17:57:57 +0530

    implement base tailwind config and style guide

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="z-10 max-w-5xl  items-center justify-between flex w-full">
-      hello
+      <h1 className="h1-bold">Next.js 14 WE ARE COMMING</h1>
     </main>
   );
 }
